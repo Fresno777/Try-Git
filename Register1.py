83d13368e6aca7a9b4bf2f127ac211cc8828c67b
Christ Missamou <99094682+Fresno777@users.noreply.github.com>
2022-02-15 00:45:44 +0000

    Add files via upload

---
+++ Register1.py
@@ -0,0 +1,27 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+"""def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')"""
+
+while True:
+    print("Register")
+    FirstName = ("")
+    LastName = ("")
+    Age = ("")
+    Email = ("")
+    Phone = ("")
+    Address = ("")
+    Country = ("")
+    id1 = input ("First name:")
+    id2 = input ("Last name: ")
+    id3 = ("Age: ")
+    id4 = input ("Email: ")
+    id5 = input ("Phone: ")
+    id6 = input ("Country: " )
+    print ("Welcome")
